8a3f91814912e0808efa71e2073d43e2f371fe25
URDead <97973081+benjisho@users.noreply.github.com>
2023-10-25 01:18:46 +0300

    Update moving_averages.py

--- indicators/moving_averages.py
+++ indicators/moving_averages.py
@@ -6,4 +6,31 @@ def simple_moving_average(data, window):
 def exponential_moving_average(data, span):
     return data['Close'].ewm(span=span, adjust=False).mean()
 
-# You can add more indicator functions as needed.
+def bollinger_bands(data, window, num_std_dev=2):
+    sma = simple_moving_average(data, window)
+    rolling_std = data['Close'].rolling(window=window).std()
+    upper_band = sma + (rolling_std * num_std_dev)
+    lower_band = sma - (rolling_std * num_std_dev)
+    return upper_band, lower_band
+
+def relative_strength_index(data, window):
+    price_diff = data['Close'].diff()
+    gain = price_diff.where(price_diff > 0, 0)
+    loss = -price_diff.where(price_diff < 0, 0)
+    avg_gain = gain.rolling(window=window).mean()
+    avg_loss = loss.rolling(window=window).mean()
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
+
+def stochastic_oscillator(data, window, window_slow=3):
+    lowest_low = data['Low'].rolling(window=window).min()
+    highest_high = data['High'].rolling(window=window).max()
+    k = 100 * (data['Close'] - lowest_low) / (highest_high - lowest_low)
+    d = k.rolling(window=window_slow).mean()
+    return k, d
+
+def stochastic_rsi(data, window, window_slow=3):
+    rsi = relative_strength_index(data, window)
+    k_rsi, d_rsi = stochastic_oscillator(pd.DataFrame({'Close': rsi}), window, window_slow)
+    return k_rsi, d_rsi
