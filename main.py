528cbd0ea55b16cc88dec759ed45771520aedeb7
URDead <97973081+benjisho@users.noreply.github.com>
2023-10-25 01:08:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 import pandas as pd
 from strategy.stock_strategy import recommend_action
+import requests
 
-# Get user input
-ticker = input("Enter a stock ticker: ")
+# Function to fetch historical stock price data based on the user's input
+def get_historical_data(symbol):
+    # Replace 'YOUR_API_KEY' with your actual Alpha Vantage API key
+    api_key = 'YOUR_API_KEY'
+    endpoint = f'https://www.alphavantage.co/query'
+    
+    # Define the parameters for the API request
+    params = {
+        'function': 'TIME_SERIES_DAILY',
+        'symbol': symbol,
+        'apikey': api_key
+    }
 
-# Load historical data
-data = pd.DataReader(ticker, 'yahoo')
+    try:
+        response = requests.get(endpoint, params=params)
+        data = response.json()
+        
+        # Convert the data into a DataFrame
+        if 'Time Series (Daily)' in data:
+            data = data['Time Series (Daily)']
+            df = pd.DataFrame(data).T
+            df.reset_index(inplace=True)
+            df.rename(columns={'index': 'Date', '1. open': 'Open', '2. high': 'High', '3. low': 'Low', '4. close': 'Close', '5. volume': 'Volume'}, inplace=True)
+            df['Date'] = pd.to_datetime(df['Date'])
+            return df
+        else:
+            print(f"Historical data for {symbol} not found.")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None
 
-# Save the historical data to a CSV file
-data.to_csv('data/historical_data.csv', index=False)
+# Ask the user for a stock symbol
+user_symbol = input("Enter the stock symbol (e.g., AAPL): ").upper()
 
-# Calculate the recommendation
-action = recommend_action(data)
+# Fetch historical data for the user-provided symbol
+data = get_historical_data(user_symbol)
 
-# Output the recommendation
-print(f"Recommendation for {ticker}: {action} for the next week")
+if data is not None:
+    # Calculate the recommendation
+    action = recommend_action(data)
+
+    # Output the recommendation
+    print(f"Recommendation for {user_symbol}: {action} for the next week")
