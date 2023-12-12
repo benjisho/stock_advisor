f9bf4184781e4ba9c91a7a28f1ca2e07548ccd1b
BenjaminS <97973081+benjisho@users.noreply.github.com>
2023-12-12 22:20:14 +0200

    Refactor stock data fetching and prediction logic
    Details:
    
    - Replaced Alpha Vantage with yfinance for fetching historical stock data.The yfinance library provides additional data such as dividends and stock splits.
    - Refactored the get_historical_data function to use yfinance and handle its data format.
    - Added a check to ensure data is not empty before proceeding with data manipulation and model training.
    Implemented a Linear Regression model to predict future stock prices based on historical data.
    - Added logic to predict stock prices for tomorrow, next week, and next month.
    - Integrated the recommend_action function from the stock_strategy module to provide investment recommendations based on the predicted prices.
    - Added user prompts for stock symbol input and displayed the fetched data and recommendations.

--- main.py
+++ main.py
@@ -4,37 +4,26 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 
 # Function to fetch historical stock price data based on the user's input
-def get_historical_data(symbol):
-    # Replace 'YOUR_API_KEY' with your actual Alpha Vantage API key
-    api_key = 'YOUR_API_KEY'
-    endpoint = f'https://www.alphavantage.co/query'
-    
-    # Define the parameters for the API request
-    params = {
-        'function': 'TIME_SERIES_DAILY',
-        'symbol': symbol,
-        'apikey': api_key
-    }
+import yfinance as yf
 
+def get_historical_data(symbol):
     try:
-        response = requests.get(endpoint, params=params)
-        data = response.json()
+        stock = yf.Ticker(symbol)
+        # data including 'Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'
         
-        # Convert the data into a DataFrame
-        if 'Time Series (Daily)' in data:
-            data = data['Time Series (Daily)']
-            df = pd.DataFrame(data).T
-            df.reset_index(inplace=True)
-            df.rename(columns={'index': 'Date', '1. open': 'Open', '2. high': 'High', '3. low': 'Low', '4. close': 'Close', '5. volume': 'Volume'}, inplace=True)
-            df['Date'] = pd.to_datetime(df['Date'])
-            return df
-        else:
+        data = stock.history(period="max")  # Fetches the maximum available historical data
+
+        # Check if the data is empty
+        if data.empty:
             print(f"Historical data for {symbol} not found.")
             return None
+        else:
+            print(f"Successfully fetched historical data for {symbol}")
+            data.reset_index(inplace=True)  # Reset the index to make 'Date' a column
+            return data
     except Exception as e:
         print(f"An error occurred: {str(e)}")
         return None
-
 # Ask the user for a stock symbol
 user_symbol = input("Enter the stock symbol (e.g., AAPL): ").upper()
 print("----------------------------------------------------------------")
@@ -43,6 +32,7 @@ print("----------------------------------------------------------------")
 data = get_historical_data(user_symbol)
 
 if data is not None:
+    # Import the necessary libraries
     # Prepare the data for machine learning
     data['Date_Num'] = (data['Date'] - data['Date'].min()).dt.days  # Convert date to numerical format
     X = data[['Date_Num']].values
@@ -64,10 +54,13 @@ if data is not None:
     next_week_date = data['Date_Num'].max() + 7
     next_month_date = data['Date_Num'].max() + 30
     
+    tomorrow_date = datetime.today() + timedelta(days=1)
+    next_week_date = datetime.today() + timedelta(days=7)
+    next_month_date = datetime.today() + timedelta(days=30)
+
     price_tomorrow = round(model.predict(np.array([[tomorrow_date]]))[0], 2)
     price_next_week = round(model.predict(np.array([[next_week_date]]))[0], 2)
-    price_next_month = round(model.predict(np.array([[next_month_date]]))[0], 2)
-    
+    price_next_month = round(model.predict(np.array([[next_month_date]]))[0], 2)    
     from strategy.stock_strategy import recommend_action
 
     # Calculate the recommendation for tomorrow, next week, and next month
